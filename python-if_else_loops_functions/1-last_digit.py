a530933263f945503e7453a0743a9ecb27dafcff
lijobazar <lijobazar@hotmail.com>
2022-09-23 15:52:38 -0700

    orrection Correct output - positive

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     lastn = number % 10
 if lastn > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, lastn)
-elif lastn == 0:
+if lastn == 0:
     print("Last digit of {} is {} and is 0".format(number, lastn))
-elif lastn < 6 and lastn != 0:
+if lastn < 6 and lastn != 0:
     print("Last digit of {} is {} and is less than \
 6 and not 0".format(number, lastn))
