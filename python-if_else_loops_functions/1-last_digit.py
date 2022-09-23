d8b997950d6383908b4f3c4a0fdaa299c2edc434
lijobazar <lijobazar@hotmail.com>
2022-09-23 15:45:10 -0700

    Upload logic at last_digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastn = ((number * -1) % 10) * -1
+else:
+    lastn = number % 10
+if lastn > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, lastn)
+elif lastn == 0:
+    print("Last digit of {} is {} and is 0".format(number, lastn))
+elif lastn < 6 and lastn != 0:
+    print("Last digit of {} is {} and is less than \
+6 and not 0".format(number, lastn))
