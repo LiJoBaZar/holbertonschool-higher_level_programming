2f87a9f1453cf8cca8e99307820241dd8b894189
lijobazar <lijobazar@hotmail.com>
2022-10-07 08:35:51 -0700

    Upload logic at advance task two

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that multiplies matrices using Numpy
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies using Numpy"""
+    return numpy.matmul(m_a, m_b)
