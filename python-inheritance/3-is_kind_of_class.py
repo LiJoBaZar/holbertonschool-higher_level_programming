97d03eb42aac46bef03b71ef9093609a0d6202a0
lijobazar <lijobazar@hotmail.com>
2022-10-14 14:07:28 -0700

    Upload solution task 4

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns True if the object is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """"Verify of object is an instance of a class"""
+    return isinstance(obj, a_class)
