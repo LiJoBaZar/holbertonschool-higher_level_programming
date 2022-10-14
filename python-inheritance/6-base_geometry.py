bf15231e7b87499036a6f40e865316c5dfa335e5
lijobazar <lijobazar@hotmail.com>
2022-10-14 14:10:55 -0700

    Upload solution task 7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Empty class BaseGeometry that defines a BaseGeometry"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+    def area(self):
+        """ Error message"""
+        raise Exception("area() is not implemented")
