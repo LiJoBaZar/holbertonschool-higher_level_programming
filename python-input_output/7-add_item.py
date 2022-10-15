37033cccc9e22590538127c497dd0d5565527143
lijobazar <lijobazar@hotmail.com>
2022-10-14 18:42:40 -0700

    correction output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-""""adds all arguments to a Python list"""
-
-import sys
-from unicodedata import name
-
+"""adds all arguments to a Python list, and then save them to a file"""
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
-
-    name = "add_item.json"
-    arg = sys.argv[1:]
+    import json
+    from sys import argv
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json = __import__('8-load_from_json_file').load_from_json_file
 
     try:
-        a = load_from_json_file(name)
-
-    except FileNotFoundError:
-        a = []
-
-    a.extend(arg)
-    save_to_json_file(a, name)
+        list_r = load_from_json('add_item.json')
+    except:
+        list_r = []
+    for arg in argv[1:]:
+        list_r.append(arg)
+    save_to_json_file(list_r, "add_item.json")
