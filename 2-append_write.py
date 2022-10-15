4ec2920b849910f3b76097de7fa3baee4520aae8
Johanna Balcazar <106547874+LiJoBaZar@users.noreply.github.com>
2022-10-14 21:02:34 -0500

    Delete 2-append_write.py

--- 2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"function that appand a string to a text file"
-
-
-def append_write(filename="", text=""):
-    "function to appand"
-    with open(filename, mode='a', encoding="utf-8") as f:
-        p = f.write(text)
-    return len(text)
