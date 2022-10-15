da6bab16bf0c04a119cb89b76203ddf49fe4e60d
lijobazar <lijobazar@hotmail.com>
2022-10-14 19:25:57 -0700

    correction output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """
- save items to a file.
- """
+Module 7-add_item
+Use with two lasted modules
+"""
+import sys
 
- import sys
- import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
- args = sys.argv[1:]
-
- save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
- load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
- item = []
- if os.path.exists("./add_item.json"):
-    item = load_from_json_file("add_item.json")
-
-    save_to_json_file(item + args, "add_item.json")
+open("add_item.json", "a")
+try:
+        file = load_from_json_file("add_item.json")
+except (ValueError):
+        file = []
+        save_to_json_file(file + sys.argv[1:], "add_item.json")
