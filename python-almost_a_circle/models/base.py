eacac56d127b864bc7d7b26d539e8441cad303fb
lijobazar <lijobazar@hotmail.com>
2022-10-21 13:07:35 -0700

    correction output

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,35 +19,37 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
-        if list_objs is None:
-            list_objs = []
-
-        filename = f"{cls.__name__}.json"
+        filename = cls.__name__ + ".json"
         with open(filename, "w") as f:
-            lst_j = list(map(lambda obj: obj.to_dictionary(), list_objs))
-            f.write(cls.to_json_string(lst_j))
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_j = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(list_j))
 
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation"""
-        if json_string is None or json_string == "":
+        if json_string is None or "":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        dummy_instance = cls(**dictionary)
-        dummy_instance.update(**dictionary)
-
-        return dummy_instance
+        if cls.__name__ == "Rectangle":
+            dummy = cls(4, 2)
+        elif cls.__name__ == "Square":
+            dummy = cls(4)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
