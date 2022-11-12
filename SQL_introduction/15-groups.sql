8555860453cb9f765d680ae1aea0c7896cda3073
lijobazar <lijobazar@hotmail.com>
2022-11-11 16:52:05 -0800

    Correction output

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
 -- print with AS function
 -- This sql at number by score with AS attribute
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
+	SELECT score, COUNT(*) AS number
+	FROM second_table
+	GROUP BY score
+	ORDER BY score DESC;
