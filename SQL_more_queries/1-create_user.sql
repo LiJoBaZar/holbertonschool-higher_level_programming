7352a908401a7af2245e8c2b07a627d43a1539c5
lijobazar <lijobazar@hotmail.com>
2022-11-16 09:16:03 -0800

    correction permisions

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Root user privileges
 -- This SQL create user woth all privileges
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d1'@'localhost';
+FLUSH PRIVILEGES;
