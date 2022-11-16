fdfce39e88924f32fb61bc86a8981e643b1cabec
lijobazar <lijobazar@hotmail.com>
2022-11-16 09:49:35 -0800

    Correction output

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,3 @@
--- cities table
--- This SQL create a db and table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256)
-	);
+-- Cities of california
+-- This SQL list cities of california in a table
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY cities.id ASC;
