230fe5ace0e89da942b35af5c6a690e546b47597
lijobazar <lijobazar@hotmail.com>
2022-11-16 14:28:06 -0800

    Correction output

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,5 @@
--- Always a name
---This SQL create a table force_name
-CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256) NOT NULL)
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS 'force_name' (
+	'id'   INT,
+	'name' VARCHAR(256) NOT NULL
+	);
