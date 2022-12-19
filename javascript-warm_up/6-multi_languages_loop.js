df2f791e62e0f9ae6e923358f4adf098ee1f5876
lijobazar <lijobazar@hotmail.com>
2022-12-19 15:56:28 -0800

    Upload task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//This a script in JavaScript that uses an array of strings and a loop to print 3 lines.
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
