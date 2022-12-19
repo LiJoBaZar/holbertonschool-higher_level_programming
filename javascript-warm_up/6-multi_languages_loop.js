064bc1666dec51c70348995b458cd41560850781
lijobazar <lijobazar@hotmail.com>
2022-12-19 15:58:06 -0800

    Correction

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//This a script in JavaScript that uses an array of strings and a loop to print 3 lines.
+// This a script in JavaScript that uses an array of strings and a loop to print 3 lines.
 
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < lines.length; i++) {
