b96c2fb851ee5172d0d30450e8d2f114df9ab436
lijobazar <lijobazar@hotmail.com>
2022-12-19 12:49:01 -0800

    Correction correct output

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-//let a = 'C is fun'/n 'Python is cool' /n 'JavaScript is amazing';
-let str = "C is fun\nPython is cool\nJavaScript is amazing";
+const str = "C is fun\nPython is cool\nJavaScript is amazing";
 console.log(str);
