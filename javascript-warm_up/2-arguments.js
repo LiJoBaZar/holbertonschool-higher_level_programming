8d60793c639c5c5a72acd1f0353de29c1e8887b6
lijobazar <lijobazar@hotmail.com>
2022-12-19 15:10:39 -0800

    Correction output

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,10 +4,10 @@ const { argv } = require('process');
 // Print a message depending on the number of arguments
 
 if (argv.length > 3) {
-console.log('No arguments');
+console.log('No argument');
 } 
 if (argv.length === 3) {
-console.log('Arguments found');
+console.log('Argument found');
 }
 if (argv.length < 3){
 console.log('Arguments found');
