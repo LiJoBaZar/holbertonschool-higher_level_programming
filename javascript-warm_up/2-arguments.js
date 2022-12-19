4ef0bd8a39810f54635ef6497f40a0552113f858
lijobazar <lijobazar@hotmail.com>
2022-12-19 15:06:19 -0800

    Upload task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { argv } = require('process');
+// Get the number of arguments passed to the script
+// Print a message depending on the number of arguments
+
+if (argv.length > 3) {
+console.log('No argument');
+} 
+if (argv.length === 3) {
+console.log('Argument found');
+}
+if (argv.length < 3){
+console.log('Arguments found');
+}
