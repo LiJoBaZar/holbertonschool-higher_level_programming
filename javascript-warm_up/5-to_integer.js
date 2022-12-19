696e703eb5bbfa4ff4ebd5b7ee53dca8f106e2eb
lijobazar <lijobazar@hotmail.com>
2022-12-19 15:48:21 -0800

    Upload task 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Get the first and second arguments passed to the script
+// Print the two arguments in the specified format, or a message if no arguments were passed
+
+const { argv } = require('process');
+const n = parseInt(argv[2]);
+if (isNaN(n)) {
+  console.log('Not a number');
+}
+else {
+  console.log('My number: ' + n);
+}
