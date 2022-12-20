588c6bfc7d91add4db5121669c9c5999d3276000
lijobazar <lijobazar@hotmail.com>
2022-12-19 16:03:39 -0800

    Upload task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const n = parseInt(argv[2]);
+
+if (!n) {
+  console.log('Missing number of occurrences');
+}
+
+for (let i = 0; i < n; i++) {
+  console.log("C is fun");
+}
