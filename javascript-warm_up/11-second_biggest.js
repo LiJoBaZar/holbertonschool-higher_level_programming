1522cae85fe1a836def630c6e592cc4d590e2907
lijobazar <lijobazar@hotmail.com>
2022-12-23 10:58:20 -0800

    Correction semistandard validation

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const second = [0, 0];
 let secondSorted;
 
 for (let i = 2; i < process.argv.length; i++) {
-	  second[i] = parseInt(process.argv[i]);
+  second[i] = parseInt(process.argv[i]);
 }
 secondSorted = second.sort(function (a, b) { return a - b; });
 secondSorted = secondSorted.reverse();
