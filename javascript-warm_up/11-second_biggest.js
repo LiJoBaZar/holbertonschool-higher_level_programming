43cd93ccaa7869918e94cd80bfd1542fc7dc0619
lijobazar <lijobazar@hotmail.com>
2022-12-23 10:55:37 -0800

    Correction output

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const second = [0, 0];
+let secondSorted;
+
+for (let i = 2; i < process.argv.length; i++) {
+	  second[i] = parseInt(process.argv[i]);
+}
+secondSorted = second.sort(function (a, b) { return a - b; });
+secondSorted = secondSorted.reverse();
+
+console.log(secondSorted[1]);
